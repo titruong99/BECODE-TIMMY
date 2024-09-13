6965396375c0e81dea918919bf765ca12627981c
Timmy <timmy2O.10@hotmail.com>
2024-09-13 12:33:02 +0200

    add of exercice 5 for advanced algo

--- Algos/advanced/advanced_5.js
+++ Algos/advanced/advanced_5.js
@@ -1,7 +1,13 @@
 const reverse=array=>{
-
+    for(let i=0;i<Math.floor(array.length/2);i++){
+        let temp=array[array.length-(i+1)];
+        array[array.length-(i+1)]=array[i];
+        array[i]=temp;
+    }
+    return array;
 }
 
-console.log(reverse([0,1,2,3,4,5,4,7])){
-
-}
+console.log(reverse([0,1,2,3,4,5,6]));
+console.log(reverse(["a","b"]));
+console.log(reverse([0,1,2,3,4]));
+console.log(reverse(["yo","salut","bonjour","hey","coucou","wassup"]));
