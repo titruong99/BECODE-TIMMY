5741afe0b940990ccc73b07bbf290b6c8f5737b6
Timmy <timmy2O.10@hotmail.com>
2024-09-20 12:28:04 +0200

    remake of exercice 0,4 of serie2

--- Algos/serie2/serie2_0.js
+++ Algos/serie2/serie2_0.js
@@ -1,10 +1,5 @@
-const countTrue=(array)=>{
-    let count=0;
-    array.forEach(element => {
-        element?count++:count;
-    });
-    return count;
-}
+const countTrue=(array)=>array.filter(element =>element==true).length;
+
 
 console.log(countTrue([true, false, false, true, false]));
 console.log(countTrue([false, false, false, false]));
--- Algos/serie2/serie2_4.js
+++ Algos/serie2/serie2_4.js
@@ -1,10 +1,13 @@
-const arrayOfMultiples=(num,length)=>{
-    let array=[];
-    for(let i=1;i<=length;i++){
-        array.push(num*i);
-    }
-    return array;
-};
+// const arrayOfMultiples=(num,length)=>{
+//     let array=[];
+//     for(let i=1;i<=length;i++){
+//         array.push(num*i);
+//     }
+//     return array;
+// };
+
+const arrayOfMultiples = (num, length) =>
+    Array.from({ length }, (_, i) => num * (i + 1));
 
 console.log(arrayOfMultiples(7, 5));
 console.log(arrayOfMultiples(12, 10));
