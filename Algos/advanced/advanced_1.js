651cb36ac4cbddcd54e38be0dcb22b32af09d3f3
Timmy <timmy2O.10@hotmail.com>
2024-09-20 12:08:07 +0200

    remake of the exercise 3 of the serie 1

--- Algos/advanced/advanced_1.js
+++ Algos/advanced/advanced_1.js
@@ -1,21 +1,21 @@
 // peut améliorer
 
-const randomNote=(noteText,magazineText)=>{
-    let count=0
-    let arrNoteText=noteText.split(" ");
-    let arrMagazineText=magazineText.split(" ");
-    for(let i=0;i<arrNoteText.length;i++){
-        let found=false;
-        for(let j=0;j<arrMagazineText.length;j++){
-            if(!found && arrNoteText[i]==arrMagazineText[j]){
-                count++;
-                arrMagazineText.splice(j,1);
-                found=true;
-            }
-        }
-    }
-    return count==arrNoteText.length;
-}
+// const ransomNote=(noteText,magazineText)=>{
+//     let count=0
+//     let arrNoteText=noteText.split(" ");
+//     let arrMagazineText=magazineText.split(" ");
+//     for(let i=0;i<arrNoteText.length;i++){
+//         let found=false;
+//         for(let j=0;j<arrMagazineText.length;j++){
+//             if(!found && arrNoteText[i]==arrMagazineText[j]){
+//                 count++;
+//                 arrMagazineText.splice(j,1);
+//                 found=true;
+//             }
+//         }
+//     }
+//     return count==arrNoteText.length;
+// }
 
 let noteText = "this is a secret note to you from a secret admirer"
 // let noteText= "this is a note to you from a secret admirer"
@@ -39,4 +39,4 @@ let magazineText = "puerto rico is a great place you must hike far from town to 
 // let magazineText = "this is my advice to you"
 
 
-console.log(randomNote(noteText,magazineText));
+console.log(ransomNote(noteText,magazineText));
--- Algos/serie1/serie1_3.js
+++ Algos/serie1/serie1_3.js
@@ -1,14 +1,16 @@
-const minMax=(array)=>{
-    let min=array[0];
-    let max=array[0];
-    if(array.length>1){
-        for(let i=0;i<array.length;i++){
-            min=array[i]<min?array[i]:min;
-            max=array[i]>max?array[i]:max;
-        }
-    }
-    return [min,max];
-}
+// const minMax=(array)=>{
+//     let min=array[0];
+//     let max=array[0];
+//     if(array.length>1){
+//         for(let i=0;i<array.length;i++){
+//             min=array[i]<min?array[i]:min;
+//             max=array[i]>max?array[i]:max;
+//         }
+//     }
+//     return [min,max];
+// }
+
+const minMax=(array)=>[Math.min(...array),Math.max(...array)];
 
 console.log(minMax([1,2,3,4,5]));
 console.log(minMax([2334454,5]));
