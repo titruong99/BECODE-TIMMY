c851ad1fbfdc74b7157c9e52c87eb15134261780
Timmy <timmy2O.10@hotmail.com>
2024-09-25 14:50:06 +0200

    remake of exercice 1 with different versions of advanced

--- Algos/advanced/advanced_1.js
+++ Algos/advanced/advanced_1.js
@@ -1,5 +1,3 @@
-// peut améliorer
-
 // const ransomNote=(noteText,magazineText)=>{
 //     let count=0
 //     let arrNoteText=noteText.split(" ");
@@ -16,6 +14,67 @@
 //     }
 //     return count==arrNoteText.length;
 // }
+
+
+// version Maxime
+// function ransomNote(word1,word2) {
+
+//     wordinaire1=word1.split(" ")
+//     wordinaire2= word2.split(" ")
+    
+//     for (let i = 0; i< wordinaire1.length; i++) {
+//         const mot= wordinaire1[i]
+//         if (!wordinaire2.includes(mot)){
+//             return false
+//         }
+//         else{
+//             const ind= wordinaire2.indexOf(mot)
+//             wordinaire2.splice(ind,1)
+//          }
+    
+// }return true}
+
+// version Ilias
+// function ransomNote(noteText, magazineText) {
+//     let noteArray = noteText.split(" ");
+//     let magazineArray = magazineText.split(" ");
+
+
+
+//     for (let i = 0; i < noteArray.length; i++) {
+
+//     let indexMot = magazineArray.indexOf(noteArray[i])
+//         if (indexMot === -1) {
+//             return false;
+
+//         } else{ 
+//         magazineArray.splice(indexMot, 1)
+//         }
+// }
+//    return true;
+
+// }
+
+const ransomNote=(noteText,magazineText)=>{
+    let arrNoteText=noteText.split(" ");
+    let arrMagazineText=magazineText.split(" ");
+    let obj={};
+    arrMagazineText.forEach(element =>obj[element]?obj[element]++:obj[element]=1);
+    console.log(obj);
+    return checkAllWordsAvailable(arrNoteText,obj);
+}
+
+const checkAllWordsAvailable=(arrayNote,objMagazine)=>{
+    for(let word of arrayNote){
+        if(objMagazine[word] && objMagazine[word]>0){
+            objMagazine[word]--;
+        }else{
+            return false
+        }
+    }
+    return true;
+}
+
 
 let noteText = "this is a secret note to you from a secret admirer"
 // let noteText= "this is a note to you from a secret admirer"
