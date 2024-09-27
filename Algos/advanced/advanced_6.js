e1f8e7f67db9e353981b2084892a93aec005e3d1
Timmy <timmy2O.10@hotmail.com>
2024-09-27 14:58:51 +0200

    correction of exercise 6 of the advanced serie

--- Algos/advanced/advanced_6.js
+++ Algos/advanced/advanced_6.js
@@ -1,7 +1,7 @@
 const twoSums=(array,sum)=>{
     let sums=[];
     for(let i=0;i<array.length;i++){
-        for(let j=i+1;j<array.length;j++){
+        for(let j=i;j<array.length;j++){
             if(array[i]+array[j]==sum){
                 sums.push([array[i],array[j]]);
             }
@@ -11,3 +11,6 @@ const twoSums=(array,sum)=>{
 }
 
 console.log(twoSums([1,6,4,3,5,7,8,9,6,3,40],10));
+console.log(twoSums([1,6,4,3,5,7,8,9,6,3,40],15));
+console.log(twoSums([1,6,4,3,5,7,8,9,6,3,40],2));
+console.log(twoSums([1,6,4,3,5,7,8,9,6,3,40],3));
