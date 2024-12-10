aeb7450fea0b204b62c2773523681d8e02ba977d
Timmy <timmy2O.10@hotmail.com>
2024-12-10 14:04:31 +0100

    api parts for adding, update and delete

--- React/toDoExercise/src/ToDo.jsx
+++ React/toDoExercise/src/ToDo.jsx
@@ -1,35 +1,122 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Activity from "./Activity";
 
 function ToDo(){
-    const[nbActivity,setNbActivity]=useState(1);
     const [toDos,setToDos]=useState([]);
+    const[nbActivity,setNbActivity]=useState(1);
     const [toDo,setToDo]=useState("");
     const handleValueInput=(e)=>{
         setToDo(e.target.value);
     }
 
+    const getLastIdToDo=()=>{
+      fetch('http://localhost:5000/todos')
+      .then(response => response.json())
+      .then(data => {
+        setNbActivity(data.length==0?1:parseInt(data[data.length-1].id)+1);
+      })
+      .catch(error => console.error("Erreur :", error));
+      
+    }
+
+    const getToDosDB=(data)=>{
+      let array=[];
+      data.map(d=>{
+        console.log(d.id);
+        array.push({id:d.id,name:d.name});
+      }
+        );
+      setToDos(array);
+    }
+
+    useEffect(() => {
+      getLastIdToDo();
+      return () => {
+      };
+    }, []);
+
+    
+    
+    const addToDoDB = () => {
+      fetch("http://localhost:5000/todos", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json"
+          },
+          body: JSON.stringify({id:nbActivity+"", todo: toDo })
+      })
+          .then(response => {
+              if (!response.ok) {
+                  throw new Error(`Erreur HTTP : ${response.status}`);
+              }
+          })
+          .catch(error => {
+              console.error("Erreur lors de l'ajout :", error);
+          });
+  };
+  
+
     const addToDo=()=>{
         if(toDo!=="" && toDo!==undefined){
             const newActivity = { name: toDo, nb: nbActivity };
             setToDos((prevToDos) => [...prevToDos, newActivity]);
+            addToDoDB();
             setNbActivity((prev) => prev + 1);
         }
-        console.log(toDos);
+    }
+
+    const updateToDoDB=(id,newActi)=>{
+      const url = `http://localhost:5000/todos/${id}`;
+      fetch(url, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: id,
+          todo: newActi,
+        }),
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Failed to update. Status: ${response.status}`);
+        }
+      })
+      .catch(error => {
+        console.error("Error updating todo:", error);
+      });
     }
 
     const updateToDo=(i,acti)=>{
         setToDos(toDos.map(t => t.nb === i ? { ...t, name: acti } : t));
+        updateToDoDB(i,acti);
+    }
+
+    const deleteToDoDB=(i)=>{
+      fetch('http://localhost:5000/todos/'+i, {
+        method: 'DELETE',
+      })
     }
 
     const deleteToDo=(i)=>{
         let newTodos=toDos.filter(t=>t.nb!==i)
-        setToDos(newTodos);       
+        setToDos(newTodos);
+        deleteToDoDB(i);       
+    }
+
+    const clearToDosDB=()=>{
+      fetch('http://localhost:5000/todos')
+      .then(response => response.json())
+      .then(data => {
+        data.forEach(todo => deleteToDoDB(todo.id));
+      })
+      .catch(error => console.error("Erreur :", error));
     }
 
     const clearToDos=()=>{
         setToDos([]);
-        setNbActivity(0);
+        setNbActivity(1);
+        clearToDosDB();
     }
 
     return (<>
